2672ee484e2edaf082924b0516c22e022c81cef4
HeytorPires <heytor.cacho29@gmail.com>
2024-09-12 16:46:53 -0300

    adicionando branch de ORM

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,24 @@
-import { server } from "./server/server";
+import express from "express";
+import "dotenv/config"; // Importando variáveis de ambiente
+import "./server/shared/services/Translations"; // Importando tradução dos erros yup
+import { router } from "./server/shared/http/routes";
+import { AppDataSource } from "./data-source";
 
-server.listen(process.env.PORT || 3333, () =>
-  console.log(`Escutando na Porta: ${process.env.PORT}`)
-);
+// Inicializando o banco de dados
+AppDataSource.initialize()
+  .then(() => {
+    // Se a conexão for bem-sucedida, inicializa o servidor
+    const server = express();
+
+    // Middlewares
+    server.use(express.json());
+    server.use(router);
+
+    const port = process.env.PORT || 3000; // Porta definida no .env ou 3000 como padrão
+    server.listen(port, () => {
+      console.log(`Servidor rodando na porta ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Erro ao inicializar o banco de dados:", error);
+  });
