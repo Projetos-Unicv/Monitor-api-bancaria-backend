9d32a7a5398128e31d6ba44e28bc381aba7cb310
HeytorPires <heytor.cacho29@gmail.com>
2024-09-20 10:40:29 -0300

    criando tabelas no DB

--- src/data-source.ts
+++ src/data-source.ts
@@ -13,10 +13,10 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  // synchronize: false, // Desative synchronize para gerar migrações corretamente
+  synchronize: false, // Desative synchronize para gerar migrações corretamente
   // entities: [`${__dirname}+server/shared/database/entities/Bank.{ts,js}`],
-  entities: ["./src/server/shared/database/entities/*.{ts,js}"],
-  migrations: ["./src/server/shared/database/migration/*.{ts,js}"],
-
+  entities: [`${__dirname}/server/shared/database/entities/*.{ts,js}`],
+  migrations: [`${__dirname}/server/shared/database/migrations/*.{ts,js}`],
+  migrationsTableName: "Migrations",
   // logging: true, // Habilite o logging para depuração, se necessário
 });
