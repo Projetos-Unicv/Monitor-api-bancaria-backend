9acd0a2af564f31e7074c8fbcfcce0adc37adc77
HeytorPires <heytor.cacho29@gmail.com>
2024-10-02 08:43:19 -0300

    :sparkles: feat: Convertendo Padrão do horario

--- src/server/modules/Record/service/getRecordService.ts
+++ src/server/modules/Record/service/getRecordService.ts
@@ -2,23 +2,36 @@ import { Record } from '../../../shared/database/entities/Record';
 import { RecordRepository } from '../repository/RecordRepository';
 import { GetBankService } from '../../Bank/service/getBankService';
 import { FilterTimes } from '../enums/FilterTimes';
+import { formatarDataParaBrasil } from '../../../shared/services/ConvertData';
+import { recordInterface } from '../../../shared/interfaces/record-Interface';
+
 export class GetRecordsService {
-  async execute(
-    bank: string,
-    type: string,
-    filter: FilterTimes | undefined
-  ): Promise<Record[]> {
+  async execute(bank: string, type: string, filter: FilterTimes | undefined) {
     const servicebank = new GetBankService();
     const banco = await servicebank.execute(bank);
     const Idbank = banco.id;
-    var limit = 0;
-    if (filter == 'DAY') {
+
+    // Definindo o limite com base no filtro
+    let limit = 0;
+    if (filter === 'DAY') {
       limit = 288;
-    } else if (filter == 'WEEK') {
+    } else if (filter === 'WEEK') {
       limit = 2016;
     } else {
       limit = 8640;
     }
-    return await RecordRepository.ListRecords(Idbank, type, limit);
+
+    const result = await RecordRepository.ListRecords(Idbank, type, limit);
+
+    //percorre e verifica se tem pelo menos um registro
+    if (result && result.length > 0) {
+      //percorre todo o registro
+      const registrosFormatados = result.map((record) => ({
+        ...record,
+        dateCreated: formatarDataParaBrasil(record.dateCreated),
+      }));
+      return registrosFormatados;
+    }
+    return;
   }
 }
