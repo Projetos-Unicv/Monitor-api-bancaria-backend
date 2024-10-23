1137ce59233a81b01f5b4757fdb4954adb5f857f
HeytorPires <heytor.cacho29@gmail.com>
2024-10-23 10:21:02 -0300

    :bug: fix: retorno do horario

--- src/server/shared/services/ConvertData.ts
+++ src/server/shared/services/ConvertData.ts
@@ -1,3 +1,6 @@
 export const formatarDataParaBrasil = (data: Date): string => {
-  return data.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+  const dataLocal = new Date(data.getTime() - data.getTimezoneOffset() * 60000);
+  return dataLocal.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
 };
+
+console.log();
