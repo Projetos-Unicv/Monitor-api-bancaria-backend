4cdb9e9fbe944735cf2d41892519d780eb9fd88e
HeytorPires <heytor.cacho29@gmail.com>
2024-11-20 20:22:19 -0300

    :bug: fix: produção e desenvolvimento para swagger

--- src/server/shared/http/routes.ts
+++ src/server/shared/http/routes.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express';
+// import { StatusCodes } from "http-status-codes";
 import { consultaRouter } from '../../modules/Record/routes/routes';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
-
-// import { StatusCodes } from "http-status-codes";
-const swaggerFilePath = path.join(__dirname, '../../docs/swagger.json');
+import 'dotenv/config'; // Importando variáveis de ambiente
 
 export const router = Router();
+
+let path_swagger;
+if (process.env.NODE_ENV === 'production') {
+  path_swagger = '/server/docs/swagger.js';
+} else {
+  path_swagger = '../../docs/swagger.json';
+}
+const swaggerFilePath = path.join(__dirname, path_swagger);
+
+router.use(
+  '/api-docs',
+  swaggerUI.serve,
+  swaggerUI.setup(require(swaggerFilePath))
+);
 
 router.get('/', (_, res) => {
   //resposta ao conectar
@@ -15,9 +28,3 @@ router.get('/', (_, res) => {
 
 // router.use("/registro", registroRouter);
 router.use('/', consultaRouter);
-
-router.use(
-  '/api-docs',
-  swaggerUI.serve,
-  swaggerUI.setup(require(swaggerFilePath))
-);
